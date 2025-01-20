d7568f3b04d14aa6898eeb00a1163a06f68ff510
Shaik Asif <bhanurshaikasif@gmail.com>
2025-01-20 11:56:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', () => {
+  const message = document.createElement('h1'); // Create an <h1> element
+  message.textContent = 'DOM load success'; // Set the text content
+  document.body.appendChild(message); // Append it to the body
+});
